a90ce535827c5d5a5593881d2377feccebae491d
NASTIIA0412 <anastasiianikitenko@gmail.com>
2023-11-02 15:25:58 +0200

    Changed keyword

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
           <img src={logo} className="App-logo img-fluid " alt="logo" />
         </header>
         <main>
-          <Dictionary defaultKeyword="cute" />
+          <Dictionary defaultKeyword="art" />
         </main>
         <footer className="App-footer">Coded by NastiiaNikitenko 🖤</footer>
       </div>
